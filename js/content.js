c969704777151378c1841b2d479c5324adf927ce
Willem Allan <willemarf@outlook.com>
2024-06-21 10:21:44 -0300

    update more values in velocity

--- js/content.js
+++ js/content.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $.each([1, 1.25, 1.50, 1.75, 2, 2.25, 2.50, 2.75, 3], function (index, value) {
+    $.each([0.80, 0.85, 0.90, 1, 1.25, 1.50, 1.75, 2, 2.25, 2.50, 2.75, 3], function (index, value) {
         chrome.storage.local.get(['key'], function (result) {
             $("#rumble_speed").append('<option value="' + value + '" ' + ((value == result.key) ? 'selected' : '') + ' >' + value + '</option>');
         });
